2b9d616c797d20093b04f9d11b9b5c49e3c9bbdc
rongxuanzhou <1152424559@qq.com>
2023-01-10 05:43:32 +0800

    webpack打包发布部分

--- webpack_jichu/change_rows_color(gehangbianse)/webpack.config.js
+++ webpack_jichu/change_rows_color(gehangbianse)/webpack.config.js
@@ -1,3 +1,6 @@
+//导入node.js中专门操作路径的模块，用require导入
+const path = require('path')
+
 // 导入html-webpack-plugin这个插件，获得构造函数
 const HtmlPlugin = require('html-webpack-plugin')
 //new 构造函数，创建插件的实例对象
@@ -7,12 +10,25 @@ const htmlPlugin = new HtmlPlugin({
     filename: './index.html'
 })
 
+//注意，左侧的{}符合是解构赋值
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 // 使用Node.js中的导出语法，向外导出一个 webpack的配置对象
 module.exports = {
     //代表 webpack 运行的模式，可选值有 development 和 production
     mode: 'development',
+    //entry：指定要处理哪个文件
+    //__dirname代表当前文件的存放路径
+    entry: path.join(__dirname, './src/index.js'),
+    //指定生成的文件要存放到哪里
+    output: {
+        //存放的目录
+        path: path.join(__dirname, 'dist'),
+        //生成的文件名
+        filename: 'js/main.js'
+    },
     //插件的数组，将来 webpack 在运行时，会加载调用的这些插件
-    plugins: [htmlPlugin],
+    plugins: [htmlPlugin, new CleanWebpackPlugin()],
     devServer: {
         open: true,
         port: 80
@@ -25,7 +41,8 @@ module.exports = {
             {test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader']},
             //处理图片文件的loader
             //如果需要调用的loader只有一个，则只传递一个字符串，有多个则用数组
-            {test: /\.jpg|png|gif$/, use: 'url-loader'},
+            //在配置url-loader的时候，多个参数之间，使用&符号进行分隔
+            {test: /\.jpg|png|gif$/, use: 'url-loader?limit=100&outputPath=images'},
             //使用babel-loader处理高级的js语法
             //在配置babel-loader的时候，程序员只需把自己的代码进行转换即可；一定要排除node_modules目录中的JS文件
             //因为第三方包中的JS兼容性，不需要程序员关心
